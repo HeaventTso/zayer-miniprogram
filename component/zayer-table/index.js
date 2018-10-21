57563a18f5a1865812095ba296d1bcc0526ac7e3
HeaventTso <z.z.heavent@hotmail.com>
2018-10-21 16:45:21 +0800

    增加表格内容滚动高度设置

--- component/zayer-table/index.js
+++ component/zayer-table/index.js
@@ -1,6 +1,10 @@
 Component({
   externalClasses: ['custom-class'],
   properties: { 
+    tbodyHeight: {
+      type: String,
+      value: "20vh"
+    },
     table: {
       type: Object, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
       value: {
@@ -14,7 +18,7 @@ Component({
     }
   },
   data: {
-    
+
   },
   ready: function () {
     this.setData({
